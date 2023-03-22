b0c391de388a57fdc07c7116ea17fa26fa14d5de
Dominic Böttger <dominic.boettger@inspirationlabs.com>
2023-03-22 16:07:38 +0000

    feat: Added Defender exclusions to Deployscript

--- AvDFsLogixDeploy.ps1
+++ AvDFsLogixDeploy.ps1
@@ -15,6 +15,8 @@ param(
     [string]$user,
     [Parameter(Mandatory=$true)]
     [string]$secret
+    [Parameter(Mandatory=$true)]
+    [string]$sharename
 )
 
 New-Item -Path "HKLM:\SOFTWARE" -Name "FSLogix" -ErrorAction Ignore
@@ -61,4 +63,38 @@ New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Lsa" -Name "LsaCf
 Add-LocalGroupMember -Group "FSLogix ODFC Exclude List" -Member "azure"
 Add-LocalGroupMember -Group "FSLogix Profile Exclude List" -Member "azure"
 
-write-host "The script has finished."
+# Defender Exclusions for FSLogix
+$Cloudcache = $true # Set for true if using cloud cache
+
+$filelist = `
+"%ProgramFiles%\FSLogix\Apps\frxdrv.sys", `
+"%ProgramFiles%\FSLogix\Apps\frxdrvvt.sys", `
+"%ProgramFiles%\FSLogix\Apps\frxccd.sys", `
+"%TEMP%\*.VHD", `
+"%TEMP%\*.VHDX", `
+"%Windir%\TEMP\*.VHD", `
+"%Windir%\TEMP\*.VHDX", `
+"\\$fileserver\$sharename\*.VHD", `
+"\\$fileserver\$sharename\*.VHDX"
+
+$processlist = `
+"%ProgramFiles%\FSLogix\Apps\frxccd.exe", `
+"%ProgramFiles%\FSLogix\Apps\frxccds.exe", `
+"%ProgramFiles%\FSLogix\Apps\frxsvc.exe"
+
+Foreach($item in $filelist){
+    Add-MpPreference -ExclusionPath $item}
+Foreach($item in $processlist){
+    Add-MpPreference -ExclusionProcess $item}
+
+If ($Cloudcache){
+    Add-MpPreference -ExclusionPath "%ProgramData%\FSLogix\Cache\*.VHD"
+    Add-MpPreference -ExclusionPath "%ProgramData%\FSLogix\Cache\*.VHDX"
+    Add-MpPreference -ExclusionPath "%ProgramData%\FSLogix\Proxy\*.VHD"
+    Add-MpPreference -ExclusionPath "%ProgramData%\FSLogix\Proxy\*.VHDX"
+    Add-MpPreference -ExclusionPath "D:\FSLogix\Cache\*.VHD"
+    Add-MpPreference -ExclusionPath "D:\FSLogix\Cache\*.VHDX"
+    Add-MpPreference -ExclusionPath "D:\FSLogix\Proxy\*.VHD"
+    Add-MpPreference -ExclusionPath "D:\FSLogix\Proxy\*.VHDX"}
+
+shutdown -r -t 0
