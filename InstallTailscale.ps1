f779f9f757ad128e055e4b7f288f22753bfd3b56
Dominic Böttger <dominic.boettger@inspirationlabs.com>
2023-05-06 17:00:30 +0000

    feat: Aded tailscale install

---
+++ InstallTailscale.ps1
@@ -0,0 +1,35 @@
+param(
+    [string]$Authkey
+)
+
+if (-not $Authkey) {
+    Write-Error "Authkey parameter is missing."
+    Exit 1
+}
+
+# Download the latest Tailscale client MSI
+$TailscaleUrl = 'https://pkgs.tailscale.com/stable/tailscale-setup-latest-amd64.msi'
+$TailscalePath = "$env:TEMP\tailscale.msi"
+Invoke-WebRequest -Uri $TailscaleUrl -OutFile $TailscalePath
+
+# Install the Tailscale client using the MSI, allow incoming connections, and start Tailscale after installation
+$InstallerArgs = @(
+    "/i",
+    "`"$TailscalePath`"",
+    "/quiet",
+    "/norestart",
+    "Tgit pS_ADMINCONSOLE=hide",
+    "TS_ALLOWINCOMINGCONNECTIONS=always",
+    "TS_KEYEXPIRATIONNOTICE=24h",
+    "TS_NETWORKDEVICES=hide",
+    "TS_TESTMENU=hide",
+    "TS_UPDATEMENU=hide",
+    "TS_UNATTENDEDMODE=always"
+)
+Start-Process -FilePath "msiexec.exe" -ArgumentList $InstallerArgs -Wait
+
+# Set the Tailscale authkey and start Tailscale
+& "C:\Program Files\Tailscale\tailscale.exe" up --authkey=$Authkey --accept-routes
+
+# Clean up the downloaded MSI
+Remove-Item $TailscalePath
